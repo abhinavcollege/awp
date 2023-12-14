2eb3aea5e2e7f00336b20e5c23482793268b588c
abhinavcollege <130778301+abhinavcollege@users.noreply.github.com>
2023-12-14 14:49:39 +0530

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class TS
+    {
+        public string UpperConvert(string text)
+        {
+            return text.ToUpper();
+        }
+        public string LowerConvert(string text)
+        {
+            return text.ToLower();
+        }
+    }
+}
